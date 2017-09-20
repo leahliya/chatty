de421a8ce41530afafee1f285251fee2b91cdcfe
leahliya <leahliya@gmail.com>
2017-09-20 13:41:21 -0500

    chatty messages

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 3001;
+
+const app = express();
+const messages = [];
+
+// const messages = [
+//     {
+//         message:'Hello',
+      
+//     },
+//     {
+//         message:'How are you?',
+       
+//     },
+//     {
+//         message:'How is the weather?',
+       
+//     }
+
+// ];
+
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.get('/messages', function (req, res, next) {
+    res.status(200).json({ messages: messages });
+  });
+
+app.post('/messages', function (req, res, next) {
+    messages.push({ message: req.body.message, time: new Date() });
+    res.status(200).json({ messages: messages });
+    console.log(req.body);
+  });  
+
+
+app.listen(port, () => {
+    console.log(`Listening on Port: ${port}`);
+});
